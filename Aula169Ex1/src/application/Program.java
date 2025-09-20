8ea87f3e1dac25d8e2773d209bbc658775e93259
kenji <158621972+NaviCustomize@users.noreply.github.com>
2025-09-20 15:31:38 -0400

    Desafio com interface

---
+++ Aula169Ex1/src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+public class Program {
+
+	public static void main(String[] args)  {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com os dados do contrato: ");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Data (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		System.out.print("Valor do contrato de parcelas: ");
+		Double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(number, date, totalValue);
+		
+		System.out.println("Entre com o numero de parcelas");
+		int n = sc.nextInt();
+		
+		ContractService contractService = new ContractService(new PaypalService());
+		
+		contractService.processContract(contract, n);
+		
+		System.out.println("Parcelas: ");
+		for(Installment installment : contract.getInstallment()) {
+			System.out.println(installment);
+		}
+		
+		sc.close();
+	}
+
+}
