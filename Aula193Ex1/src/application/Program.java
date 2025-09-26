5823609f967449ce28651016a500af26140d2685
kenji <158621972+NaviCustomize@users.noreply.github.com>
2025-09-26 15:22:02 -0400

    Adicao de texto explicatorio sobre expressoes lambda

--- Aula193Ex1/src/application/Program.java
+++ Aula193Ex1/src/application/Program.java
@@ -7,44 +7,47 @@ import java.util.List;
 import entities.Product;
 
 public class Program {
-	
-	public static void main(String[]args) {
-		
-		List<Product> list = new ArrayList<>();
-		
-		list.add(new Product("TV",900.00));
-		list.add(new Product("Notebook",1200.00));
-		list.add(new Product("Tablet",450.00));
-		
-		/* muito grande
-		//classe anonima
-		Comparator<Product> comp = new Comparator<Product>() {
 
-			@Override
-			public int compare(Product p1, Product p2) {
-				
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
-			
-		};
-		*/
-		//Expressao lambda tambem e funcao anonima
-		Comparator<Product> comp = (p1,p2) -> { // <- ArrowFunction
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()); //Implementacao da funcao
-		};
-		/*   Ou pode ser tambem:
-		 *   Comparator<Product> comp = (p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+	public static void main(String[] args) {
+
+		// funcao anonima é uma funcao que nao tem nome
+		// Expressoes lambda: Em programa funcional, expressao
+		// lambda corresponde a uma funcao anonima de primeira classe
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		/*
+		 * muito grande //classe anonima Comparator<Product> comp = new
+		 * Comparator<Product>() {
+		 * 
+		 * @Override public int compare(Product p1, Product p2) {
+		 * 
+		 * return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()); }
+		 * 
+		 * };
 		 */
-		
-		
-		//list.sort(new MyComparator());
+		// Expressao lambda tambem e funcao anonima
+		Comparator<Product> comp = (p1, p2) -> { // <- ArrowFunction
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()); // Implementacao da funcao
+		};
+		/*
+		 * Ou pode ser tambem: Comparator<Product> comp = (p1,p2) ->
+		 * p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		 */
+
+		// list.sort(new MyComparator());
 		list.sort(comp);
 		//
-		//list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
-		for(Product p : list) {
+		// list.sort((p1,p2) ->
+		// p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		for (Product p : list) {
 			System.out.println(p);
 		}
-		
+
 	}
-	
+
 }
