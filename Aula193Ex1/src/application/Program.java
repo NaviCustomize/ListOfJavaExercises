d32c4ea6c998b795dfc1fe247a7dd9fe88363249
kenji <158621972+NaviCustomize@users.noreply.github.com>
2025-09-25 22:49:00 -0400

    comparator in different scenarios

---
+++ Aula193Ex1/src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+	
+	public static void main(String[]args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV",900.00));
+		list.add(new Product("Notebook",1200.00));
+		list.add(new Product("Tablet",450.00));
+		
+		/* muito grande
+		//classe anonima
+		Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+			
+		};
+		*/
+		//Expressao lambda tambem e funcao anonima
+		Comparator<Product> comp = (p1,p2) -> { // <- ArrowFunction
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()); //Implementacao da funcao
+		};
+		/*   Ou pode ser tambem:
+		 *   Comparator<Product> comp = (p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		 */
+		
+		
+		//list.sort(new MyComparator());
+		list.sort(comp);
+		//
+		//list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		for(Product p : list) {
+			System.out.println(p);
+		}
+		
+	}
+	
+}
